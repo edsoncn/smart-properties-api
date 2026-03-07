9334ecd44ab83dc68917769a521a76e4ffb48c94
Edson Castillo <edsedx4@gmail.com>
2026-03-07 16:51:28 -0500

    fixed redis config to use also cluster

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -4,6 +4,7 @@ module.exports = Object.freeze({
     REDIS_HOST: process.env.REDIS_HOST ?? '0.0.0.0',
     REDIS_PORT: process.env.REDIS_PORT ?? 6379,
     REDIS_PASSWORD: process.env.REDIS_PASSWORD ?? null,
+    REDIS_CLUSTER: process.env.REDIS_CLUSTER ? process.env.REDIS_CLUSTER === 'true' : false,
     INIT_DATA_FORCE: process.env.INIT_DATA_FORCE ? process.env.INIT_DATA_FORCE === 'true' : false,
     SMARTPROPERTIES_TENANT: 'smartproperties',
     SUPER_ADMIN: 'super-admin',
