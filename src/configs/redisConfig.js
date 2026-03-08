c415a56f1f3669e568ed6066e5f9965c3c4a6965
Edson Castillo <edsedx4@gmail.com>
2026-03-07 21:25:07 -0500

    reverted: fixed redis config to use also cluster

--- src/configs/redisConfig.js
+++ src/configs/redisConfig.js
@@ -2,21 +2,10 @@ const redis = require('redis')
 const constants = require('../helpers/constants')
 
 
-const client = constants.REDIS_CLUSTER ? 
-        redis.createCluster({
-            rootNodes: [
-                {
-                    url: `redis://${constants.REDIS_HOST}:${constants.REDIS_PORT}`
-                }
-            ],
-            defaults: {
-                password: constants.REDIS_PASSWORD
-            }
-        }) : 
-        redis.createClient({ 
-            url: `redis://${constants.REDIS_HOST}:${constants.REDIS_PORT}`,
-            password: constants.REDIS_PASSWORD
-        })
+const client =  redis.createClient({
+    url: `redis://${constants.REDIS_HOST}:${constants.REDIS_PORT}`,
+    password: constants.REDIS_PASSWORD 
+})
 
 module.exports = {
     client
